48600e669ee0918497a42881ca9c20c34e8fbdce
sschachenhofer <34660993+sschachenhofer@users.noreply.github.com>
2018-02-17 18:29:16 +0100

    Added method simon()

--- Test.java
+++ Test.java
@@ -8,8 +8,13 @@ public class HelloPeople {
 		System.out.println("Schöne Grüße von Tobias");
 	}
 	
+	public static void simon() {
+		System.out.println("Schöne Grüße von Simon");
+	}
+	
 	public static void main(String[] args) {	
 		emre();
 		tobias();
+		simon();
 	}
 }
